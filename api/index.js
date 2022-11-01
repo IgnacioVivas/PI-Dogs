18299bce99a0095fc1eeff62b23e2d063301d68a
IgnacioVivas <ignacab@hotmail.com>
2022-11-01 18:07:45 -0300

    connecting with railway

--- api/index.js
+++ api/index.js
@@ -17,13 +17,12 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const { DB_PORT } = require('./src/config.js');
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(DB_PORT, () => {
+  server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
--- api/src/db.js
+++ api/src/db.js
@@ -3,11 +3,18 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 // const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
-const { DB_USER, DB_PASSWORD, DB_HOST } = require('./config');
+// const { DB_USER, DB_PASSWORD, DB_HOST, DB_URL } = require('./config');
+const { DB_URL } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/railway`, {
+const sequelize = new Sequelize(DB_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 const basename = path.basename(__filename);
 
