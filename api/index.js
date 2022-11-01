bfb631ec7792095b0ccad2282a29868a29d7b396
IgnacioVivas <ignacab@hotmail.com>
2022-10-31 22:53:22 -0300

    deploy db

--- api/index.js
+++ api/index.js
@@ -17,12 +17,13 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const { DB_PORT } = require('./src/config.js');
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
+  server.listen(DB_PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
---
+++ api/src/config.js
@@ -0,0 +1,12 @@
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_USER = process.env.DB_USER || 'postgres';
+const DB_PASSWORD = process.env.DB_PASSWORD || 'homeroaleli';
+// const DB_NAME = process.env.DB_NAME ||
+const DB_PORT = process.env.DB_PORT || 3001;
+
+module.exports = {
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_PORT,
+};
